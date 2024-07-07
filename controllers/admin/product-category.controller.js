c46134de9f20ef5da5ba66e22d91cbda4af5397e
hieu22pham <phammhieu2211@gmail.com>
2024-07-07 22:37:11 +0700

    update management category

--- controllers/admin/product-category.controller.js
+++ controllers/admin/product-category.controller.js
@@ -1,5 +1,30 @@
+const ProductCategory = require("../../models/product-category.model")
+
+
 module.exports.index = (req, res) => {
   res.render("admin/pages/product-category/index", {
     pageTitle: "Danh mục sản phẩm"
   })
+}
+
+module.exports.create = (req, res) => {
+  res.render("admin/pages/product-category/create", {
+    pageTitle: "Tạo danh mục sản phẩm"
+  })
+}
+
+module.exports.createPost = async (req, res) => {
+
+  if (req.body.position == "") {
+    const countProducts = await ProductCategory.countDocuments();
+
+    req.body.position = countProducts + 1
+  } else {
+    req.body.position = parseInt(req.body.position)
+  }
+
+  const record = new ProductCategory(req.body)
+  await record.save()
+
+  res.redirect("/admin/products-category")
 }
--- routes/admin/product-category.route.js
+++ routes/admin/product-category.route.js
@@ -2,7 +2,22 @@ const express = require('express')
 const router = express.Router();
 
 const controller = require("../../controllers/admin/product-category.controller")
+const validate = require("../../validates/admin/product-category.validate")
+const uploadCloud = require("../../middlewares/admin/uploadCloud.middlewares")
+
+const multer = require("multer")
+const upload = multer()
 
 router.get("/", controller.index)
+
+router.get("/create", controller.create)
+router.post('/create',
+  upload.single("thumbnail"),
+  uploadCloud.upload,
+  validate.createPost,
+  controller.createPost)
+
+
+
 
 module.exports = router
