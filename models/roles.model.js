53af8944d1d717819c31f75a57f1e269b7ffb35f
hieu22pham <phammhieu2211@gmail.com>
2024-07-13 23:18:11 +0700

    update code roles

--- models/roles.model.js
+++ models/roles.model.js
@@ -1,9 +1,6 @@
 const mongoose = require("mongoose");
-var slug = require("mongoose-slug-updater")
 
-mongoose.plugin(slug)
-
-const productSchema = new mongoose.Schema(
+const rolesSchema = new mongoose.Schema(
   {
     title: String,
     product_category_id: String,
