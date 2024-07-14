e20f06afca53ddff20b1794d3a6bdbac989e2879
hieu22pham <phammhieu2211@gmail.com>
2024-07-14 23:33:32 +0700

    update code

--- controllers/admin/role.controller.js
+++ controllers/admin/role.controller.js
@@ -4,49 +4,8 @@ const searchHelper = require("../../helpers/search")
 const paginationHelper = require("../../helpers/pagination")
 
 module.exports.index = async (req, res) => {
-  const filterStatus = filterStatusHelper(req.query)
 
-  let find = {
-    deleted: false
-  }
-  if (req.query.status) {
-    find.status = req.query.status;
-  }
-
-  const objectSearch = searchHelper(req.query)
-
-  if (objectSearch.regex) {
-    find.title = objectSearch.regex
-  }
-
-  // pagination
-  const countProduct = await Products.countDocuments(find)
-
-  let objectPagination = paginationHelper({
-    currentPage: 1,
-    limitItem: 4
-  }, req.query, countProduct
-  )
-
-  console.log(objectPagination.currentPage)
-  // end pagination
-
-  //sort
-  let sort = {}
-
-  if (req.query.sortKey && req.query.sortValue) {
-    sort[req.query.sortKey] = req.query.sortValue
-  } else {
-    sort.position = "desc"
-  }
-
-  //end sort
-
-  const products = await Products.find(find).sort(sort)
-    .limit(objectPagination.limitItem)
-    .skip(objectPagination.skip)
-
-  res.render("admin/pages/products/index", {
+  res.render("admin/pages/role", {
     pageTitle: "Danh sách sản phẩm",
     products: products,
     filterStatus: filterStatus,
