9e16031efc0388db00e006ae5a9daf751142d103
hieu22pham <phammhieu2211@gmail.com>
2024-07-14 23:18:37 +0700

    update code

--- routes/admin/index.route.js
+++ routes/admin/index.route.js
@@ -3,6 +3,7 @@ const productRoutes = require("./product.route")
 
 const systemConfig = require("../../config/system/system.js")
 const productCategoryRoutes = require("./product-category.route.js")
+const roleRoutes = require("../../controllers/admin/role.controller")
 
 module.exports = (app) => {
   const PATH_ADMIN = systemConfig.prefixAdmin
@@ -10,4 +11,6 @@ module.exports = (app) => {
   app.use(PATH_ADMIN + '/dashboard', dashboardRoutes)
   app.use(PATH_ADMIN + '/products', productRoutes)
   app.use(PATH_ADMIN + '/products-category', productCategoryRoutes)
+  app.use(PATH_ADMIN + '/roles', roleRoutes)
+
 }
