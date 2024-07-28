8c4f13f8f4d9013cab656eaadc0a02ae7d22c85e
hieu22pham <phammhieu2211@gmail.com>
2024-07-28 23:48:58 +0700

    update roles.js

--- public/admin/js/roles.js
+++ public/admin/js/roles.js
@@ -1,1 +1,12 @@
 const tablePermissions = document.querySelector("[table-permissions]")
+if (tablePermissions) {
+  const buttonSubmit = document.querySelector("[button-submit]")
+
+  buttonSubmit.addEventListener("click", () => {
+    let permissions = [];
+
+    const rows = tablePermissions.querySelectorAll("[data-name]")
+
+    
+  })
+}
