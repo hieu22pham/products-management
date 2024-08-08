f999911401d3f21d84da3ae82123e9f7aba98d4a
hieu22pham <phammhieu2211@gmail.com>
2024-08-08 23:01:03 +0700

    update autho

--- middlewares/admin/auth.middlewares.js
+++ middlewares/admin/auth.middlewares.js
@@ -1,5 +1,6 @@
 const systemConfig = require("../../config/system/system")
 const Account = require("../../models/account.model")
+const Role = require("../../models/roles.model")
 
 module.exports.requireAuth = async (req, res, next) => {
   if (!req.cookies.token) {
@@ -11,6 +12,7 @@ module.exports.requireAuth = async (req, res, next) => {
       res.redirect(`${systemConfig.prefixAdmin}/auth/login`)
     }
     else {
+      const Role = await Role.findOne({ _id: user.role_id })
       res.locals.user = user
 
       next()
