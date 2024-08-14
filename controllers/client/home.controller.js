349bcb90f6a7f751c508c6b1c04c5688a4ddeee3
hieu22pham <phammhieu2211@gmail.com>
2024-08-14 21:43:22 +0700

    update code

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,5 +1,17 @@
-module.exports.index = (req, res) => {
+const ProductCategory = require("../../models/product-category.model")
+const createTreeHelper = require("../../helpers/createTree")
+
+module.exports.index = async (req, res) => {
+  const productsCategory = await ProductCategory.find({
+    deleted: false
+  })
+
+  const newProductsCategory = createTreeHelper.tree(productsCategory)
+
+  console.log(newProductsCategory)
+
   res.render("client/pages/home/index", {
-    pageTitle: "Trang chủ"
+    pageTitle: "Trang chủ",
+    layoutProductsCategory: newProductsCategory
   })
 }
