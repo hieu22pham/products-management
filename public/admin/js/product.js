2773e76a60a4b4e0434ebc195406ce9b02802f78
hieu22pham <phammhieu2211@gmail.com>
2024-08-14 14:57:01 +0700

    update code

--- public/admin/js/product.js
+++ public/admin/js/product.js
@@ -121,21 +121,7 @@ if (buttonDelete.length > 0) {
 }
 // end delete products
 
-// show alert
-const showAlert = document.querySelector("[show-alert]")
-if (showAlert) {
-  const time = parseInt(showAlert.getAttribute("data-time"))
-  const closeAlert = showAlert.querySelector("[close-alert]")
 
-  setTimeout(() => {
-    showAlert.classList.add("alert-hidden")
-  }, time)
-
-  closeAlert.addEventListener("click", () => {
-    showAlert.classList.add("alert-hidden")
-  })
-}
-// end show alert
 
 
 const buttonDeleteBin = document.querySelectorAll("[button-delete-bin]")
