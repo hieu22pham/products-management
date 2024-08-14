4c21955d0359f0b721097310232a269074c33c77
hieu22pham <phammhieu2211@gmail.com>
2024-08-14 14:50:03 +0700

    update code

--- controllers/admin/my-account.controller.js
+++ controllers/admin/my-account.controller.js
@@ -1,3 +1,33 @@
+const Account = require("../../models/account.model")
+
 module.exports.index = async (req, res) => {
   res.render("admin/pages/my-account/index")
+}
+
+module.exports.edit = async (req, res) => {
+  res.render("admin/pages/my-account/edit",
+    {
+      pageTitle: "Chỉnh sửa thông tin cá nhân"
+    }
+  )
+}
+
+module.exports.editPatch = async (req, res) => {
+  const id = res.locals.user.id
+
+  const emailExist = await Account.findOne({
+    _id: { $ne: id },
+    email: req.body.email,
+    deleted: false
+  })
+
+  if (emailExist) {
+    req.flash("error", `Email ${req.body.email} đã tồn tại`)
+    res.redirect("back")
+    return;
+  }
+  await Account.updateOne({ _id: id }, req.body)
+  req.flash("success", "Cập nhật tài khoản thành công!")
+
+  res.redirect("back")
 }
