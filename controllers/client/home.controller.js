3cf1d943088b187f3019d171b8d20a31c83e2439
hieu22pham <phammhieu2211@gmail.com>
2024-08-14 22:04:22 +0700

    update code

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,17 +1,8 @@
-const ProductCategory = require("../../models/product-category.model")
-const createTreeHelper = require("../../helpers/createTree")
+
 
 module.exports.index = async (req, res) => {
-  const productsCategory = await ProductCategory.find({
-    deleted: false
-  })
-
-  const newProductsCategory = createTreeHelper.tree(productsCategory)
-
-  console.log(newProductsCategory)
 
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
-    layoutProductsCategory: newProductsCategory
   })
 }
--- routes/client/index.route.js
+++ routes/client/index.route.js
@@ -1,10 +1,11 @@
 const productRouter = require("./products.route")
 const homeRouter = require("./home.route")
+const categoryMiddleware = require("../../middlewares/client/category.middlewares")
 
 module.exports = (app) => {
-  app.use('/', homeRouter)
+  app.use('/', categoryMiddleware.category, homeRouter)
 
 
 
-  app.use('/products', productRouter)
+  app.use('/products', categoryMiddleware.category, productRouter)
 }
