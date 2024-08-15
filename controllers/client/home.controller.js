94a10cdaee6804b572f084de64bd9519a7d3e742
hieu22pham <phammhieu2211@gmail.com>
2024-08-15 21:56:35 +0700

    update code

--- controllers/client/home.controller.js
+++ controllers/client/home.controller.js
@@ -1,8 +1,14 @@
-
+const Product = require("../../models/product.model")
 
 module.exports.index = async (req, res) => {
+  const producsFeatured = await Product.find({
+    deleted: false,
+    featured: "1",
+    status: "active"
+  })
 
   res.render("client/pages/home/index", {
     pageTitle: "Trang chủ",
+    producsFeatured: producsFeatured
   })
 }
