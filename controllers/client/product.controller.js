36c92bdcbf8fb742a46a267b47c8049beef91e98
hieu22pham <phammhieu2211@gmail.com>
2024-08-17 23:18:19 +0700

    update code

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -54,7 +54,7 @@ module.exports.category = async (req, res) => {
   } else {
     products = await Product.find({
       deleted: false,
-    })
+    }).sort({ position: "desc" })
   }
 
   const newProducts = productsHelper.priceNewProducts(products)
