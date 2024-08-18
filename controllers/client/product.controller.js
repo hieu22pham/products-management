6d2b5abf16ebe3f07b9a1d612554243e5a69569b
hieu22pham <phammhieu2211@gmail.com>
2024-08-18 22:58:43 +0700

    update code

--- controllers/client/product.controller.js
+++ controllers/client/product.controller.js
@@ -75,6 +75,7 @@ module.exports.category = async (req, res) => {
   const newProducts = productsHelper.priceNewProducts(products)
 
   res.render("client/pages/products/index.pug", {
+    pageTitle: category.title,
     products: newProducts
   })
 }
