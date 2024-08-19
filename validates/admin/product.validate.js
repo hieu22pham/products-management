5140d8b7c22cb08d34d3b6fbc5a354a178b4f5b9
hieu22pham <phammhieu2211@gmail.com>
2024-08-19 23:57:59 +0700

    update code

--- validates/admin/product.validate.js
+++ validates/admin/product.validate.js
@@ -7,6 +7,7 @@ module.exports.createPost = (req, res, next) => {
   }
 
   if (!req.body.product_category_id) {
+    req.flash("error", "Vui lòng chọn danh mục!");
     res.redirect("back")
 
     return;
