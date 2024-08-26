e5592a0ae27303fcaacf771f3d550b344bf42477
hieu22pham <phammhieu2211@gmail.com>
2024-08-26 23:37:37 +0700

    update code

--- middlewares/client/cart.middlewares.js
+++ middlewares/client/cart.middlewares.js
@@ -1,5 +1,14 @@
+const Cart = require("../../models/cart.model")
+
 module.exports.cartId = (req, res, next) => {
-  console.log("Luôn chạy")
+  console.log(req.cookies.cardId)
+  if (!req.cookies.cardId) {
+    const cart = new Cart()
+    await cart.save()
+  } else {
+
+  }
+
 
   next()
 }
