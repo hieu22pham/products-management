e443b7ff21409282fc6311c3d30baf82f1283215
hieu22pham <phammhieu2211@gmail.com>
2024-08-26 23:05:25 +0700

    update code

--- controllers/client/search.controller.js
+++ controllers/client/search.controller.js
@@ -1,5 +1,25 @@
+const Product = require("../../models/product.model")
+const productsHelper = require("../../helpers/products")
+
 module.exports.index = async (req, res) => {
+  const keyword = req.query.keyword;
+  if (keyword) {
+    const keywordRegex = new RegExp(keyword, "i")
+
+    const products = await Product.find({
+      title: keywordRegex,
+      status: "active",
+      deleted: false
+    })
+
+    const newProducts = productsHelper.priceNewProducts(products)
+    console.log(newProducts)
+  }
+
+
   res.render("client/pages/search/index.pug", {
     pageTitle: "Tìm kiếm sản phẩm",
+    keyword: keyword,
+    products: newProducts
   })
 }
