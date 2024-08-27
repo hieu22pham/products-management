008096dfab27ca02f53e8671c49b69082fa8ca04
hieu22pham <phammhieu2211@gmail.com>
2024-08-27 23:42:27 +0700

    update code

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,4 +1,16 @@
+const Cart = require("../../models/cart.model")
+
 // [POST] /cart/add/:productId
-module.exports.addPost = () => {
-  res.send("Ok")
+module.exports.addPost = (req, res) => {
+  const cartId = req.cookies.cartId
+  const productId = req.params.productId
+  const quantity = parseInt(req.body.quantity)
+
+  console.log(cartId)
+  console.log(productId)
+  console.log(quantity)
+
+  const
+
+    res.send("Ok")
 }
