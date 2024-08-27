ff739c4493022a0ce303fa2ff35914545af63364
hieu22pham <phammhieu2211@gmail.com>
2024-08-27 23:44:46 +0700

    update code

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -1,16 +1,25 @@
 const Cart = require("../../models/cart.model")
 
 // [POST] /cart/add/:productId
-module.exports.addPost = (req, res) => {
+module.exports.addPost = async (req, res) => {
   const cartId = req.cookies.cartId
   const productId = req.params.productId
   const quantity = parseInt(req.body.quantity)
 
+  const objectCart = {
+    product_id: productId,
+    quantity: quantity
+  }
   console.log(cartId)
   console.log(productId)
   console.log(quantity)
 
-  const
+  await Cart.updateOne({
+    _id: cartId
+  },
+    {
+      $push: { products: objectCart }
+    })
 
-    res.send("Ok")
+  res.redirect("back")
 }
