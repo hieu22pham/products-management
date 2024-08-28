66685b69fc2af59c258d996087fa1898b0cf4c32
hieu22pham <phammhieu2211@gmail.com>
2024-08-28 23:39:45 +0700

    update code

--- controllers/client/cart.controller.js
+++ controllers/client/cart.controller.js
@@ -7,21 +7,32 @@ module.exports.addPost = async (req, res) => {
     const productId = req.params.productId
     const quantity = parseInt(req.body.quantity)
 
-    const objectCart = {
-      product_id: productId,
-      quantity: quantity
-    }
-    console.log("card id:", cartId)
-    console.log("productId", productId)
-    console.log("quantity", quantity)
-
-    await Cart.updateOne({
+    const cart = await Cart.findOne({
       _id: cartId
-    },
-      {
-        $push: { products: objectCart }
+    })
+
+    const existProductInCart = cart.products.find(item => item.product_id)
+
+    if (existProductInCart) {
+      const newQuantity = 
+    } else {
+      const objectCart = {
+        product_id: productId,
+        quantity: quantity
       }
-    )
+      console.log("card id:", cartId)
+      console.log("productId", productId)
+      console.log("quantity", quantity)
+
+      await Cart.updateOne({
+        _id: cartId
+      },
+        {
+          $push: { products: objectCart }
+        }
+      )
+    }
+
     req.flash("success", "Thêm vào giỏ hàng thành công!")
     res.redirect("back")
   } catch (e) {
