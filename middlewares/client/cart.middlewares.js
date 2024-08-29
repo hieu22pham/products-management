e4da72d54592c94819146d6dfb30d3ca7917247e
hieu22pham <phammhieu2211@gmail.com>
2024-08-29 23:45:40 +0700

    update code

--- middlewares/client/cart.middlewares.js
+++ middlewares/client/cart.middlewares.js
@@ -14,6 +14,8 @@ module.exports.cartId = async (req, res, next) => {
     const cart = await Cart.findOne({
       _id: req.cookies.cartId
     })
+
+    cart.totalQuantity = cart.products.reduce((sum, item) => sum + item.quantity, 0)
   }
   next()
 }
