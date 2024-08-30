6423a02ff8dbf1098843a23356630cb42f89866b
hieu22pham <phammhieu2211@gmail.com>
2024-08-30 21:55:39 +0700

    update code

--- public/js/product.js
+++ public/js/product.js
@@ -1,4 +1,13 @@
 const cartOutOfStock = document.querySelector("[btn-cart-outOfStock]")
 if (cartOutOfStock) {
-  
+
+}
+
+const buttonsGoBack = document.querySelectorAll("[button-go-back]")
+if (buttonsGoBack.length > 0) {
+  buttonsGoBack.forEach(button => {
+    button.addEventListener("click", () => {
+      history.back()
+    })
+  });
 }
