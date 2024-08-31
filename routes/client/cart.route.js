0a3943ec63748500eaefc9653bf6dd85dc353275
hieu22pham <phammhieu2211@gmail.com>
2024-08-31 23:51:35 +0700

    update code

--- routes/client/cart.route.js
+++ routes/client/cart.route.js
@@ -5,7 +5,10 @@ const controller = require("../../controllers/client/cart.controller")
 router.get('/', controller.index)
 
 router.post('/add/:productId', controller.addPost)
+
 router.get('/delete/:productId', controller.delete)
+
+router.get('/update/:productId/:quantity', controller.update)
 
 
 
